07f3674e7b08a7e2cc429e99aa535c5f45af7d5d
KaterinaRajmanova <valentova.kacka@seznam.cz>
2021-04-11 16:26:03 +0200

    nahodny text z Listu + nahodny obrazek z webu

--- src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
+++ src/main/java/cz/czechitas/java2webapps/ukol2/controller/MainController.java
@@ -16,6 +16,7 @@ public class MainController {
 
         String citatText;
         List<String> citatyDneSeznam = new ArrayList<>();
+        List<String> obrazkyDneSeznam = new ArrayList<>();
 
         citatyDneSeznam = Arrays.asList("Největší chyba, kterou v životě můžete udělat, je mít pořád strach, že nějakou uděláte" +
                 "", "Úspěch není konečný, neúspěch není fatální: odvaha pokračovat je to, co se počítá" +
@@ -28,14 +29,20 @@ public class MainController {
                 "", "Veškerý pokrok se děje mimo zónu komfortu." +
                 "", "Soustřeďte se na to, kam chcete jít, nikoli na to, čeho se bojíte.");
 
+        obrazkyDneSeznam = Arrays.asList("CgyrwbE6Hm4","sKJ7zSylUao","MVr6pgZzlbY","YnNczu62rdk","JdjdIjzJl94","Jdem_eQTHTQ" +
+                "","k88qpEjVJ1k","JdPHvI7VF0o","tuIedADlL_Q","d1RHYJ09Ur0");
+
         Random generatorCísla = new Random();
-        int nahodneCislo = generatorCísla.nextInt(citatyDneSeznam.size());
-        citatText = citatyDneSeznam.get(nahodneCislo);
+        int nahodneCisloCitaty = generatorCísla.nextInt(citatyDneSeznam.size());
+        citatText = citatyDneSeznam.get(nahodneCisloCitaty);
+
+        int nahodneCisloObrazky = generatorCísla.nextInt(obrazkyDneSeznam.size());
+        String nahodnyObrazek = obrazkyDneSeznam.get(nahodneCisloObrazky);
 
         ModelAndView result = new ModelAndView("index");
         result.addObject("citatText", citatText);
-        result.addObject("citatObrazek",String.format("background-image: url(/images/image%d.jpg)", nahodneCislo));
-
+        result.addObject("citatObrazek","background-image: url(https://source.unsplash.com/" +
+                ""+nahodnyObrazek+"/1600x900)");
         return result;
 
 
